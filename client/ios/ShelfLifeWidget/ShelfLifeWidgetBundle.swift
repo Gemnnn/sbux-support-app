0df18c3964471b2a63e7da610ddb5f17a1ec5778
Branden Min <brandenmin@gmail.com>
2024-11-25 11:21:38 -0500

    feat: add initial widget extension
    - Added a Widget Extension target to the Xcode project.

---
+++ client/ios/ShelfLifeWidget/ShelfLifeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ShelfLifeWidgetBundle.swift
+//  ShelfLifeWidget
+//
+//  Created by Sol on 11/24/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ShelfLifeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ShelfLifeWidget()
+    }
+}
