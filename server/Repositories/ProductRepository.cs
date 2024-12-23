9340e81fc3de20c2144efaf24096b7d422a2df9d
Branden Min <brandenmin@gmail.com>
2024-12-23 15:47:41 -0500

    refactor: Simplify product search and remove caching

--- server/Repositories/ProductRepository.cs
+++ server/Repositories/ProductRepository.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using server.Data;
 using server.Models;
 
@@ -10,13 +9,11 @@ namespace server.Repositories
     public class ProductRepository : IProductRepository
     {
         private readonly ProductDbContext _context;
-        private readonly IMemoryCache _cache;
 
 
-        public ProductRepository(ProductDbContext context, IMemoryCache cache)
+        public ProductRepository(ProductDbContext context)
         {
             _context = context;
-            _cache = cache;
         }
 
         public async Task<Product> GetProductByNameAsync(string name)
@@ -27,28 +24,10 @@ namespace server.Repositories
 
         public async Task<IEnumerable<Product>> SearchProductsAsync(string partialName)
         {
-            //return await _context.Products
-            //                     .Where(p => p.ProductName.Contains(partialName))
-            //                     .ToListAsync();
-
-            // create the cache key
-            string cacheKey = $"Search_{partialName.ToLower()}";
-
-            // cache check
-            if (!_cache.TryGetValue(cacheKey, out IEnumerable<Product> cachedResults))
-            {
-                // optimize SQL LIKE query
-                cachedResults = await _context.Products
-                    .Where(p => EF.Functions.Like(p.ProductName, $"%{partialName}%"))
-                    .AsNoTracking() // No Tracking
-                    .ToListAsync();
-
-                // 10 minutes Available
-                _cache.Set(cacheKey, cachedResults, TimeSpan.FromMinutes(10));
-            }
-
-            return cachedResults;
-
+            return await _context.Products
+                .Where(p => EF.Functions.Like(p.ProductName, $"%{partialName}%"))
+                .AsNoTracking() 
+                .ToListAsync();
         }
     }
 }
